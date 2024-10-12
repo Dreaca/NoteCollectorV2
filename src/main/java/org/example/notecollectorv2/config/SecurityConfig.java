a91b2c023ef23d7ef81c39a9648d8eed1947fd81
dreaca <buddhikadreaca@gmail.com>
2024-10-12 10:31:58 +0530

    basic authentication done

--- src/main/java/org/example/notecollectorv2/config/SecurityConfig.java
+++ src/main/java/org/example/notecollectorv2/config/SecurityConfig.java
@@ -1,13 +1,25 @@
 package org.example.notecollectorv2.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Value("${secure.username}")
+    private String username;
+    @Value("${secure.password}")
+    private String password;
+    @Value("${secure.roles}")
+    private String roles;
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests()
@@ -16,4 +28,13 @@ public class SecurityConfig {
                 .httpBasic();
         return http.build();
     };
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()//principle user
+                .username(username)
+                .password(password)
+                .roles(roles)
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
 }
